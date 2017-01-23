a3377ad3ec33521487a9f7bb28d4adf9eab818a5
athanaseus <aramaila@ska.ac.za>
2017-01-23 10:55:27 +0200

    Adding register device and put device property methods to helper_module to aid in registering the correct class names when generating a tango device simulator

--- tango_simlib/helper_module.py
+++ tango_simlib/helper_module.py
@@ -1,6 +1,8 @@
 import os
 import sys
 
+import PyTango
+
 
 def get_server_name():
     """Gets the TANGO server name from the command line arguments
@@ -22,3 +24,20 @@ def get_server_name():
     executable_name = os.path.split(sys.argv[0].split('.')[0])[1]
     server_name = executable_name + '/' + sys.argv[1]
     return server_name
+
+def register_device(name, device_class, server_name, instance):
+    dev_info = PyTango.DbDevInfo()
+    dev_info.name = name
+    dev_info._class = device_class
+    dev_info.server = "{}/{}".format(server_name, instance)
+    print """Attempting to register TANGO device {!r}
+        class: {!r}  server: {!r}.""".format(
+        dev_info.name, dev_info._class, dev_info.server)
+    db = PyTango.Database()
+    db.add_device(dev_info)
+
+def put_device_property(dev_name, property_name, property_value):
+    db = PyTango.Database()
+    print "Setting device {!r} property {!r}: {!r}".format(
+          dev_name, property_name, property_value)
+    db.put_device_property(dev_name, {property_name: property_value})
