5a168bbd7302ee6adc0c615b1d279e9cf2477bf3
athanaseus <aramaila@ska.ac.za>
2017-02-01 16:49:07 +0200

    renaming attribute from relativeHumidity to relative_humidity

--- tango_simlib/tests/test_sim_xmi_parser.py
+++ tango_simlib/tests/test_sim_xmi_parser.py
@@ -312,7 +312,7 @@ class test_XmiParser(GenericSetup):
         """
         actual_parsed_attrs = self.xmi_parser.get_reformatted_device_attr_metadata()
         expected_attr_list = ['insolation', 'temperature', 'pressure', 'rainfall',
-                              'relativeHumidity', 'wind_direction', 'input_comms_ok',
+                              'relative_humidity', 'wind_direction', 'input_comms_ok',
                               'wind_speed']
         actual_parsed_attr_list = actual_parsed_attrs.keys()
         self.assertGreater(len(actual_parsed_attr_list), 0,
