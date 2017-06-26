a323b26d8ddd54540866b594364ef115efd711bf
athanaseus <aramaila@ska.ac.za>
2017-06-26 12:45:26 +0200

    Proper names of config files

--- tango_simlib/examples/weather3.py
+++ tango_simlib/examples/weather3.py
@@ -4,11 +4,10 @@ from tango_simlib.tango_sim_generator import (configure_device_model, get_tango_
 
 
 def main():
-    sim_data_files = ['../tests/weather_sim.xmi', '../tests/weather_SIMDD_3.json']
+    sim_data_files = ['../tests/weather.xmi', '../tests/Weather_SIMDD_3.json']
     model = configure_device_model(sim_data_files)
     TangoDeviceServers = get_tango_device_server(model, sim_data_files)
     server_run(TangoDeviceServers)
 
 if __name__ == "__main__":
     main()
-
