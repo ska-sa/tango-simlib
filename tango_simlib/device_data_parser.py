62ea20ac705a201718d0ff338a68aa8b0853b749
kmadisa <kmadisa@ska.ac.za>
2018-02-21 16:00:17 +0200

    Updating docstrings.

--- tango_simlib/device_data_parser.py
+++ tango_simlib/device_data_parser.py
@@ -46,78 +46,83 @@ class DeviceDataParser(object):
         self.data_description_file_name = ''
         self.device_class_name = ''
         self._device_attributes = {}
-        """The data structure format is a dict containing attribute info in a dict
+        """The data structure format is a dict containing attribute info in a dict.
+
+        e.g.
             {
-                u'<attribute_name>': {
-                    u'alarms': {
-                        u'delta_t': u'Not specified',
-                        u'delta_val': u'Not specified',
-                        u'extensions': u'[]',
-                        u'max_alarm': u'Not specified',
-                        u'max_warning': u'Not specified',
-                        u'min_alarm': u'Not specified',
-                        u'min_warning': u'Not specified'
+                'State': {
+                    'alarms': {
+                        'delta_t': 'Not specified',
+                        'delta_val': 'Not specified',
+                        'extensions': '[]',
+                        'max_alarm': 'Not specified',
+                        'max_warning': 'Not specified',
+                        'min_alarm': 'Not specified',
+                        'min_warning': 'Not specified'
                     },
-                    u'color': u'Lime',
-                    u'data_format': <tango._tango.AttrDataFormat>,
-                    u'data_type': <tango._tango.CmdArgType>,
-                    u'database': u'monctl:10000',
-                    u'description': u'',
-                    u'device': u'tango/admin/monctl',
-                    u'display_unit': u'No display unit',
-                    u'events': {
-                        u'arch_event': {
-                            u'archive_abs_change': u'Not specified',
-                            u'archive_period': u'Not specified',
-                            u'archive_rel_change': u'Not specified',
-                            u'extensions': u'[]'
+                    'color': 'Lime',
+                    'data_format': tango._tango.AttrDataFormat.SCALAR,
+                    'data_type': tango._tango.CmdArgType.DevState,
+                    'database': 'monctl:10000',
+                    'description': '',
+                    'device': 'tango/admin/monctl',
+                    'display_unit': 'No display unit',
+                    'events': {
+                        'arch_event': {
+                            'archive_abs_change': 'Not specified',
+                            'archive_period': 'Not specified',
+                            'archive_rel_change': 'Not specified',
+                            'extensions': '[]'
                         },
-                        u'ch_event': {
-                            u'abs_change': u'Not specified',
-                            u'extensions': u'[]',
-                            u'rel_change': u'Not specified'
+                        'ch_event': {
+                            'abs_change': 'Not specified',
+                            'extensions': '[]',
+                            'rel_change': 'Not specified'
                         },
-                        u'per_event': {
-                            u'extensions': u'[]',
-                            u'period': u'1000'
+                        'per_event': {
+                            'extensions': '[]',
+                            'period': '1000'
                         }
                     },
-                    u'format': u'Not specified',
-                    u'label': u'State',
-                    u'max_alarm': u'Not specified',
-                    u'min_alarm': u'Not specified',
-                    u'model': u'monctl:10000/tango/admin/monctl/State',
-                    u'name': u'State',
-                    u'polling': 1000,
-                    u'quality': u'ATTR_VALID',
-                    u'standard_unit': u'No standard unit',
-                    u'string': u'ON',
-                    u'time': 1519128820.715947,
-                    u'unit': u'',
-                    u'value': 0,
-                    u'writable': u'READ'},
+                    'format': 'Not specified',
+                    'label': 'State',
+                    'max_alarm': 'Not specified',
+                    'min_alarm': 'Not specified',
+                    'model': 'monctl:10000/tango/admin/monctl/State',
+                    'name': 'State',
+                    'polling': 1000,
+                    'quality': PyTango.AttrQuality.ATTR_VALID,
+                    'standard_unit': 'No standard unit',
+                    'string': 'ON',
+                    'time': 1519207194.715621,
+                    'unit': '',
+                    'value': 0,
+                    'writable': 'READ'},
             }
         """
         self._device_commands = {}
         """The data structure format is a dict containing command info in a dict
+        e.g.
             {
-                u'<command_name>': {
-                    'doc_in': u'<description>',
-                    'doc_out': u'<description>,
-                    'dtype_in': <tango._tango.CmdArgType>,
-                    'dtype_out': <tango._tango.CmdArgType>,
-                    'name': u'<command_name>'
+                'State': {
+                    'doc_in': 'Uninitialised',
+                    'doc_out': 'Device state',
+                    'dtype_in': tango._tango.CmdArgType.DevVoid,
+                    'dtype_out': tango._tango.CmdArgType.DevState,
+                    'name': 'State'
                 },
             }
         """
         self._device_properties = {}
-        """The data structure format is a list containing device property info in a dict
+        """The data structure format is a list containing device property info in a dict.
+        e.g.
             {
                 '<property_name>': <property_value>,
             }
         """
         self._device_class_properties = {}
-        """The data structure format is a list containing device property info in a dict
+        """The data structure format is a list containing device property info in a dict.
+        e.g.
             {
                 '<property_name>': <property_value>,
             }
@@ -177,7 +182,7 @@ class DeviceDataParser(object):
     def get_reformatted_cmd_metadata(self):
         return self._device_commands
 
-    def get_reformatted_properties_metadata(self, deviceProps):
+    def get_reformatted_properties_metadata(self, property_group):
         return self._device_properties
 
     def get_reformatted_override_metadata(self):
