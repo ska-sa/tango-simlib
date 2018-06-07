daf6ec2d2bccfaf4b7c839489ab61753228c77a0
samtwum <stwum@ska.ac.za>
2018-06-07 03:42:34 +0200

    changing function calls to cmd_override function

--- tango_simlib/tests/test_simdd_json_parser.py
+++ tango_simlib/tests/test_simdd_json_parser.py
@@ -212,7 +212,7 @@ class test_PopulateModelActions(GenericSetup):
         pmq = model.PopulateModelQuantities(self.simdd_parser, device_name)
         sim_model = pmq.sim_model
         cmd_info = self.simdd_parser.get_device_command_metadata()
-        override_info = self.simdd_parser.get_device_properties_metadata()
+        override_info = self.simdd_parser.get_device_cmd_override_metadata()
         model.PopulateModelActions(cmd_info, override_info, device_name, sim_model)
 
         actual_actions_list = sim_model.sim_actions.keys()
@@ -228,7 +228,7 @@ class test_PopulateModelActions(GenericSetup):
         pmq = model.PopulateModelQuantities(self.simdd_parser, device_name)
         sim_model = pmq.sim_model
         cmd_info = self.simdd_parser.get_device_command_metadata()
-        override_info = self.simdd_parser.get_device_properties_metadata()
+        override_info = self.simdd_parser.get_device_cmd_override_metadata()
         model.PopulateModelActions(cmd_info, override_info, device_name, sim_model)
         sim_model_actions_meta = sim_model.sim_actions_meta
 
@@ -249,7 +249,7 @@ class test_PopulateModelActions(GenericSetup):
         pmq = model.PopulateModelQuantities(self.simdd_parser, device_name)
         sim_model = pmq.sim_model
         cmd_info = self.simdd_parser.get_device_command_metadata()
-        override_info = self.simdd_parser.get_device_properties_metadata()
+        override_info = self.simdd_parser.get_device_cmd_override_metadata()
         model.PopulateModelActions(cmd_info, override_info, device_name, sim_model)
         action_on = sim_model.sim_actions['On']
         self.assertEqual(action_on.func.im_class, override_class.OverrideWeather)
@@ -259,7 +259,7 @@ class test_PopulateModelActions(GenericSetup):
         pmq = model.PopulateModelQuantities(self.simdd_parser, device_name)
         sim_model = pmq.sim_model
         cmd_info = self.simdd_parser.get_device_command_metadata()
-        override_info = self.simdd_parser.get_device_properties_metadata()
+        override_info = self.simdd_parser.get_device_cmd_override_metadata()
         model.PopulateModelActions(cmd_info, override_info, device_name, sim_model)
         action_set_temperature = sim_model.sim_actions['SetTemperature']
         data_in = 25.00
