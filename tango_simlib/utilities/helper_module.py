64a621d0a89271482fb189f71219d7f0eeb02cdb
Muhammed Toufeeq Ockards <tockards@ska.ac.za>
2019-12-05 11:55:15 +0200

    corrected docstring to a comment

--- tango_simlib/utilities/helper_module.py
+++ tango_simlib/utilities/helper_module.py
@@ -182,7 +182,7 @@ def json_loads_byteified(json_text):
 
 
 def _byteify(data, ignore_dicts=False):
-    """If this is a unicode string, return its string representation."""
+    # If this is a unicode string, return its string representation.
     if isinstance(data, unicode):
         return data.encode("utf-8")
     # if this is a list of values, return list of byteified values
