df3fe1e81c360e85ea7c534684df1da856200b50
Muhammed Toufeeq Ockards <tockards@ska.ac.za>
2019-12-19 11:59:37 +0200

    import future

--- tango_simlib/tests/test_sim_xmi_parser.py
+++ tango_simlib/tests/test_sim_xmi_parser.py
@@ -7,6 +7,7 @@
 from __future__ import absolute_import, division, print_function
 from future import standard_library
 standard_library.install_aliases()  # noqa: E402
+import future
 
 import logging
 import unittest
