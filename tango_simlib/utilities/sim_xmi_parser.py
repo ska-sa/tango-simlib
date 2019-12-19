ee619822aded3d2c6ee815025ea7fa916f8579d1
Muhammed Toufeeq Ockards <tockards@ska.ac.za>
2019-12-19 22:12:44 +0200

    remove unused import

--- tango_simlib/utilities/sim_xmi_parser.py
+++ tango_simlib/utilities/sim_xmi_parser.py
@@ -11,7 +11,6 @@ from __future__ import absolute_import, division, print_function
 from future import standard_library
 
 standard_library.install_aliases()  # noqa: E402
-import future
 from future.utils import itervalues
 
 import logging
