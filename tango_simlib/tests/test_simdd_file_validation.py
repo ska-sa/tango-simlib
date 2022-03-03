79f649d8855fbce9502290bc6d36398c881c3454
Katleho Madisa <katleho.madisa47@gmail.com>
2022-03-03 16:00:41 +0200

    #140 Fixed linting issues

--- tango_simlib/tests/test_simdd_file_validation.py
+++ tango_simlib/tests/test_simdd_file_validation.py
@@ -7,8 +7,12 @@ from jsonschema import validate
 def test_validate_testsim_simdd_file():
     """Check that the Testsim_SimDD.json file conforms to the SimDD.schema"""
 
-    simdd_schema_file = pkg_resources.resource_filename('tango_simlib.utilities', 'SimDD.schema')
-    simdd_config_file = pkg_resources.resource_filename('tango_simlib.tests.config_files', 'TestSim_SimDD.json')
+    simdd_schema_file = pkg_resources.resource_filename(
+        "tango_simlib.utilities", "SimDD.schema"
+    )
+    simdd_config_file = pkg_resources.resource_filename(
+        "tango_simlib.tests.config_files", "TestSim_SimDD.json"
+    )
 
     with open(simdd_schema_file) as simdd_schema, open(simdd_config_file) as simdd_file:
         schema_data = json.load(simdd_schema)
